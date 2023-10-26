71b61ae686fe6d366bb0ed489b15551992c0e6ba
unknown <talha98ansari@gmail.com>
2023-10-26 14:22:37 -0700

    product page added

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -189,6 +189,7 @@
     <script src="assets/js/all.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    
     <script src="assets/js/custom.js"></script>
 
 </body>
